6860bb1c7167a7db4cbab8a8b4741eb934826ef8
Vadim Bondarev <haghard84@gmail.com>
2016-01-21 13:04:36 +0300

    Debug:0

--- core/src/main/scala/microservice/api/MicroserviceKernel.scala
+++ core/src/main/scala/microservice/api/MicroserviceKernel.scala
@@ -45,13 +45,13 @@ abstract class MicroserviceKernel(override val akkaSystemPort: String,
 
     val akkaSeeds = if (clusterRole == GatewayRole) {
       Option(System.getProperty(SEEDS_ENV_VAR)).map(line => line.split(",").toList)
-        .fold(List(s"$la:$akkaSystemPort"))(identity)
-      //(s"$la:$akkaSystemPort" :: _)
-
+          .fold(List(s"$externalAddress:$akkaSystemPort"))(s"$la:$akkaSystemPort" :: _)
     } else {
       Option(System.getProperty(SEEDS_ENV_VAR))
         .fold(throw new Exception(s"$SEEDS_ENV_VAR env valuable should be defined"))(x => x.split(",").toList)
     }
+
+    println("******************************" + akkaSeeds)
 
     val seedNodesString = akkaSeeds.map { node =>
       val ap = node.split(":")
