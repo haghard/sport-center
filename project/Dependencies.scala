2580def82f2cf21c1f84cdb6149d85fd31f1280a
Vadim Bondarev <haghard84@gmail.com>
2016-02-25 17:59:20 +0300

    update nosql-join-stream

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -65,7 +65,7 @@ object Dependencies {
 
   val protobuf         = "com.google.protobuf"    %  "protobuf-java"           % "2.5.0"
 
-  val nosql_join       = "com.haghard"            %% "nosql-join-stream"       % "0.1.14"
+  val nosql_join       = "com.haghard"            %% "nosql-join-stream"       % "0.1.15"
 
   val http_session     = "com.softwaremill.akka-http-session"  %%  "core"      % "0.2.4"
 }
